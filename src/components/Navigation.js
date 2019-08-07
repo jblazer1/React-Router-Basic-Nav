22698e859545fc1e4e67350c8fde79fce933cb32
JamesB <jblazer1@yahoo.com>
2019-08-07 12:46:17 -0700

    Import dependencies, add Routes, add Link

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,4 +1,5 @@
-import React from 'react';
+import React from "react";
+import { Link, NavLink } from "react-router-dom";
 
 const Navigation = () => {
   return (
@@ -6,13 +7,13 @@ const Navigation = () => {
       <div className="App">
         <h1>React Router Mini</h1>
         <div>
-          <a href="">Home</a>
+          <NavLink to="/">Home</NavLink>
         </div>
         <div>
-          <a href="">About</a>
+          <Link to="/about">About</Link>
         </div>
         <div>
-          <a href="">Contact</a>
+          <Link to="/contact">Contact</Link>
         </div>
       </div>
     </div>
